44bd902a89c4989e5d302573e30da40dba9e4758
ariadnaali25 <ariadnaali@iresm.edu.ar>
2023-09-12 11:11:24 -0300

    12/09

--- libroide/applications/app1/forms.py
+++ libroide/applications/app1/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import Prestamo
+from .models import Prestamo, Libro
 
 class PrestamoForm(forms.ModelForm):
     class Meta:
         model = Prestamo
+        fields = '__all__'
+
+class LibroForm(forms.ModelForm):
+    class Meta:
+        model = Libro
         fields = '__all__'
--- libroide/applications/app1/views.py
+++ libroide/applications/app1/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from .models import Prestamo
-from .forms import PrestamoForm
+from .models import Prestamo, Libro
+from .forms import PrestamoForm, LibroForm
 
 class Prestamo(ListView):
     template_name ="librovich.html"
@@ -18,3 +18,18 @@ def prestamoForm(request):
     else:
         form = PrestamoForm()
     return render(request, 'index.html', {'form': form})
+
+class Libro(ListView):
+    template_name ="librovich.html"
+    model=Libro
+    context_object_name="libro"
+
+def libroForm(request):
+    if request.method == 'POST':
+        form2 = LibroForm(request.POST)
+        if form2.is_valid():
+            form2.save()
+            return redirect('listaLibro')  # Redireccionar a la lista después de guardar
+    else:
+        form2 = LibroForm()
+    return render(request, 'index.html', {'form2': form2})
