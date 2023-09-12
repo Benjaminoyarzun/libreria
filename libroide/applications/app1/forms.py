c4836ad48c9790576752c3a930fac3beb92cd6f5
Benjaminoyarzun <benjaminoyarzunlopez@iresm.edu.ar>
2023-09-12 11:45:54 -0300

    12/9

--- libroide/applications/app1/forms.py
+++ libroide/applications/app1/forms.py
@@ -5,8 +5,3 @@ class PrestamoForm(forms.ModelForm):
     class Meta:
         model = Prestamo
         fields = '__all__'
-
-class LibroForm(forms.ModelForm):
-    class Meta:
-        model = Libro
-        fields = '__all__'
